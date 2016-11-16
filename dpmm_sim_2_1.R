c4e7c45826b8b7abc2161994ecc0eb6317c02439
dcries <dcries@iastate.edu>
2016-11-16 14:40:17 -0600

    update writeups

---
+++ dpmm_sim_2_1.R
@@ -0,0 +1,9 @@
+setwd('/home/dcries')
+Rcpp::sourceCpp('ebmodel/ppred_analysis.cpp')
+Rcpp::sourceCpp('ebmodel/bivar_bvnmcmc1_qp.cpp')
+source('ebmodel/base_fcn.R')
+source("ebmodel/bvn_simulation.R")
+
+bvn_2_1 <- run_bvnsim(2,1)
+
+save(bvn_2_1,file="bvn_2_1.RData")
