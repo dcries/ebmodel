aad0063db0b2f8c8ab06d3d7bac5f1cd5b0de056
dcries <dcries@iastate.edu>
2016-11-18 09:29:14 -0600

    incorrectly used bvn functions

--- dpmm_sim_2_1.R
+++ dpmm_sim_2_1.R
@@ -1,9 +1,9 @@
 setwd('/home/dcries')
 Rcpp::sourceCpp('ebmodel/ppred_analysis.cpp')
-Rcpp::sourceCpp('ebmodel/bivar_bvnmcmc1_qp.cpp')
+Rcpp::sourceCpp('ebmodel/bivar_fullmcmc.cpp')
 source('ebmodel/base_fcn.R')
-source("ebmodel/bvn_simulation.R")
+source("ebmodel/dpmm_simulation.R")
 
-bvn_2_1 <- run_bvnsim(2,1)
+dpmm_2_1 <- run_dpmmsim(2,1)
 
-save(bvn_2_1,file="bvn_2_1.RData")
+save(dpmm_2_1,file="dpmm_2_1.RData")
